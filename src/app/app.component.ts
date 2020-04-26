6ee21b0b80fb7e4a62d4e0532a2e1454ce92f874
Nejmi Sidaty <enesssidaty@Nejmis-iMac.local>
2020-04-26 23:00:29 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {FCM} from "@ionic-native/fcm/ngx";
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,24 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm: FCM
   ) {
+    this.fcm.getToken().then(token => {
+      console.log('token is ' + token);
+    });
+
+    this.fcm.onNotification().subscribe(data => {
+      if(data.wasTapped){
+        console.log("Received in background");
+      } else {
+        console.log("Received in foreground");
+      };
+    });
+
+    this.fcm.onTokenRefresh().subscribe(token => {
+      console.log('token is ' + token);
+    });
     this.initializeApp();
   }
 
@@ -23,5 +40,7 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+
+
   }
 }
